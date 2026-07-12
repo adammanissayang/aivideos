7647732f8c358bd3e6b143a8a854d53a09182353
adammanissayang <adammanissayang.github@outlook.com>
2026-07-13 00:11:04 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,37 +1,45 @@
-console.log("AdamManis AI Studio");
+const watchBtn=document.getElementById("watchBtn");
+
+watchBtn.addEventListener("click",()=>{
+
+document.getElementById("trending").scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
 
 
 
+// Fade In
 
+const cards=document.querySelectorAll(".movieCard");
 
+const observer=new IntersectionObserver(entries=>{
 
-const API_KEY = "MASUKKAN_API_KEY_KAU"; // 🔑 letak API key TMDB
-const BASE_URL = "https://api.themoviedb.org/3";
-const IMG_URL = "https://image.tmdb.org/t/p/w500";
+entries.forEach(entry=>{
 
-const carousel = document.getElementById("carousel");
+if(entry.isIntersecting){
 
-// Fetch trending movie
-fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`)
-  .then(res => res.json())
-  .then(data => {
-    displayMovies(data.results);
-  })
-  .catch(err => console.error(err));
+entry.target.style.opacity=1;
 
-// Display movies
-function displayMovies(movies) {
-  carousel.innerHTML = "";
+entry.target.style.transform="translateY(0px)";
 
-  movies.slice(0, 10).forEach((movie, index) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    card.innerHTML = `
-      <span class="rank">${index + 1}</span>
-      <img src="${IMG_URL + movie.poster_path}" alt="${movie.title}">
-    `;
-
-    carousel.appendChild(card);
-  });
 }
+
+});
+
+});
+
+cards.forEach(card=>{
+
+card.style.opacity=0;
+
+card.style.transform="translateY(40px)";
+
+card.style.transition=".6s";
+
+observer.observe(card);
+
+});
